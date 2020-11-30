383a21cac420e816909dcba82d3731c9f1c569cc
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-11-30 16:19:31 -0500

    subscriber model created required by route

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const Subscriber = require('../models/subscriber')
 
 // Getting ALL
-router.get('/', (req, res) => {
-    res.send('hello world')
+router.get('/', async (req, res) => {
+    try {
+        const subscribers = await Subscriber.find()
+        res.json(subscribers)
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
 })
 // Getting ONE
 router.get('/:id', (req, res) => {
