41a00499ccc4e3ee70edfe79c704ba0366635c06
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-11-30 17:02:16 -0500

    REST API complete

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,27 +1,91 @@
-const express = require('express')
-const router = express.Router()
-const Subscriber = require('../models/subscriber')
+const express = require("express");
+const { get } = require("mongoose");
+const subscriber = require("../models/subscriber");
+const router = express.Router();
+const Subscriber = require("../models/subscriber");
 
 // Getting ALL
-router.get('/', async (req, res) => {
-    try {
-        const subscribers = await Subscriber.find()
-        res.json(subscribers)
-    } catch (error) {
-        res.status(500).json({
-            message: error.message
-        })
-    }
-})
-// Getting ONE
-router.get('/:id', (req, res) => {
-    res.send(req.params.id)
-})
-// Creating ONE
-router.post("/", (req, res) => {});
-// Updating ONE
-router.patch("/:id", (req, res) => {});
-// Deleting ONE
-router.delete("/:id", (req, res) => {});
+router.get("/", async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find();
+    res.json(subscribers);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
 
-module.exports = router
+// Getting ONE
+router.get("/:id", getSubscriber, (req, res) => {
+  res.json(res.subscriber);
+});
+
+// Creating ONE
+router.post("/", async (req, res) => {
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscribedToChannel: req.body.subscribedToChannel,
+  });
+  try {
+    const newSubscriber = await subscriber.save();
+    res.status(201).json(newSubscriber);
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+});
+
+// Updating ONE
+router.patch("/:id", getSubscriber, async (req, res) => {
+  if (req.body.name !== null) {
+    res.subscriber.name = req.body.name;
+  }
+  if (req.body.subscribedToChannel !== null) {
+    res.subscriber.subscribedToChannel = req.body.subscribedToChannel;
+  }
+  try {
+      const updatedSubscriber = await res.subscriber.save()
+      res.json(updatedSubscriber)
+  } catch (error) {
+      res.status(400).json({
+          message: error.message
+      })
+  }
+});
+
+// Deleting ONE
+router.delete("/:id", getSubscriber, async (req, res) => {
+  try {
+    await res.subscriber.remove();
+    res.json({
+      message: "Deleted subscriber",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+async function getSubscriber(req, res, next) {
+  let subscriber;
+  try {
+    subscriber = await Subscriber.findById(req.params.id);
+    if (subscriber === null) {
+      return res.status(404).json({
+        message: "Cannot find subscriber",
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+
+  res.subscriber = subscriber;
+  next();
+}
+
+module.exports = router;
