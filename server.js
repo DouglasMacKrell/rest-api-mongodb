f98af67484ef4b7b0b4aedc44cabe2aaaa295202
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-11-30 14:18:53 -0500

    mongoose connection

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-mongoose.connect('mongodb://localhost/subscribers')
+mongoose.connect(process.env.DATABASE_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+const db = mongoose.connection
+db.on('error', (error) => console.log(error))
+db.once('open', () => console.log('connected to database'))
+
 const PORT = 3000
 
 app.listen(PORT, () => {
